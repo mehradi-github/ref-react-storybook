61bd8974fde78fc130289c26f7b90234992a9c6e
Mehradi <mehradi.info@gmail.com>
2024-08-29 10:50:07 +0200

    Adding Theme provider in root #26

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
+import { ThemeProvider } from "styled-components";
+import useDarkMode from "use-dark-mode";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
+  const { value } = useDarkMode(false, { global: globalThis.window });
+  const theme = value ? darkTheme : lightTheme;
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <AppRoutes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }

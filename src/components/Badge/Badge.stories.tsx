1c9864069aac8016eeab568d2f75049f9a6d0b9d
mehradi <mehradi.info@gmail.com>
2024-09-21 15:54:27 +0200

    Fix error: Cannot read properties of undefined (reading 'badgeBackground') in Badge.stories #56

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,13 +1,30 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Badge } from './Badge';
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "../../styles/theme";
+import { GlobalStyle } from '../../styles/GlobalStyle';
+import useDarkMode from "use-dark-mode";
 
 const meta={
     title:'components/Badge',
     component: Badge,
     args:{
         text:'Comfort food'
-    }
-} satisfies Meta;
+    },
+    decorators:[
+        (Story,context)=>{
+
+            const { value } = useDarkMode(false, { global: globalThis.window })
+            const theme = value ? darkTheme : lightTheme
+            return (
+                <ThemeProvider theme={theme}>
+                   <GlobalStyle />
+                   <Story {...context}/>
+                </ThemeProvider>
+            )
+        }
+    ]
+} satisfies Meta<typeof Badge>;
 
 export default meta;
 
